b9586bd3d3b0d1c8a455f805cae297d16dd597fc
S.Pichaya_big <pabigmz@gmail.com>
2025-07-23 14:38:56 +0700

    เปลี่ยนรูปแบบข้อมูลที่ส่งไปบันทึก และเปลี่ยนเส้น endpoint ของ api

---
+++ src/app/services/stocktransfer.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { Observable } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StocktransferService {
+  private apiUrl = 'https://localhost:7089/api/stocktransfer';
+
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { }
+
+  transferRequest(payload: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/transfer`, payload, { headers: this.createAuthHeaders() });
+  }
+
+
+  private createAuthHeaders(): HttpHeaders {
+    let token = '';
+    if (isPlatformBrowser(this.platformId)) {
+      token = sessionStorage.getItem('token') || '';
+    }
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+  }
+}
