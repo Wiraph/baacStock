2b55f3f3c54032ccf56c6c2c023284ae289d3dba
S.Pichaya_big <pabigmz@gmail.com>
2025-09-14 18:54:17 +0700

    เพิ่มฟังก์ชัน การแสดง PDF และโหลด excel

--- src/app/services/Metadata/system-metadata.ts
+++ src/app/services/Metadata/system-metadata.ts
@@ -1,7 +1,8 @@
-import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { EncryptionService } from '../encryption.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,7 @@ export class SystemMetadata {
   private readonly apiUrl = `${environment.dotnetApiUrl}/api/SystemMetadata`;
   constructor(
     private readonly http: HttpClient,
-    @Inject(PLATFORM_ID) private readonly platformId: Object
+    private readonly encryptionService: EncryptionService
   ) { }
 
   sysCfg(): Observable<any> {
@@ -21,6 +22,26 @@ export class SystemMetadata {
 
   remCode(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/remcode`, {
+      withCredentials: true
+    });
+  }
+
+  branch(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/data`, {
+      withCredentials: true
+    });
+  }
+
+  Province(payload: any): Observable<any[]> {
+    const encryptedPayload = this.encryptionService.encrypPayload(payload);
+    return this.http.post<any[]>(`${this.apiUrl}/brprv`, encryptedPayload, {
+      withCredentials: true
+    });
+  }
+
+  BrBranch(payload: any): Observable<any[]> {
+    const encryptedPayload = this.encryptionService.encrypPayload(payload);
+    return this.http.post<any[]>(`${this.apiUrl}/brbranch`, encryptedPayload, {
       withCredentials: true
     });
   }
